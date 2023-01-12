dd28db03d461c17d7ab31f0d25715ad2ba3c9537
avery-stephens <astephens@student.neumont.edu>
2023-01-11 23:05:17 -0700

    SpaceShip added
    spaceship shoots and can rotate now

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [Range(1,15), Tooltip("controls speed of the player")] public float speed = 5f;
+    [Range(1,360)] public float rotationRate;
     public GameObject prefab;
+    public Transform bulletSpawnLocation;
 
 	private void Awake()
 	{
@@ -26,22 +28,22 @@ public class Player : MonoBehaviour
         //transform.localScale = Vector3.one * Random.value * 5;
 
         Vector3 direction = Vector3.zero;
-
-        direction.x = Input.GetAxis("Horizontal");
         direction.z = Input.GetAxis("Vertical");
 
-        //if (Input.GetKey(KeyCode.A)) direction.x = -1;
-        //if (Input.GetKey(KeyCode.D)) direction.x = +1;
-        //if (Input.GetKey(KeyCode.W)) direction.z = +1;
-        //if (Input.GetKey(KeyCode.S)) direction.z = -1;
+        Vector3 rotation = Vector3.zero;
+        rotation.y = Input.GetAxis("Horizontal");
 
-        transform.position += direction * speed * Time.deltaTime;
+        Quaternion rotate = Quaternion.Euler(rotation * rotationRate * Time.deltaTime);
+        transform.rotation = transform.rotation * rotate;
 
-        if (Input.GetButtonDown("Fire1"))
+        transform.Translate(direction * speed * Time.deltaTime);
+        //transform.position += direction * speed * Time.deltaTime;
+
+        if (Input.GetButton("Fire1"))
         {
-            Debug.Log("PEW PEW");
-            GetComponent<AudioSource>().Play();
-            Instantiate(prefab, transform.position, transform.rotation);
+            //Debug.Log("PEW PEW");
+            //GetComponent<AudioSource>().Play();
+            GameObject go = Instantiate(prefab, bulletSpawnLocation.position, bulletSpawnLocation.rotation);
         }
     }
 }
