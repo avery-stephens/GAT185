3f3d1a01d29f72fb67db1605bd58732af56f9814
avery-stephens <astephens@student.neumont.edu>
2023-01-25 11:59:14 -0700

    Various Changes Made

---
+++ Assets/Scripts/RollerCamera.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollerCamera : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField, Range(2, 20)] private float distance;
+    [SerializeField, Range(20, 80)] private float pitch;
+    [SerializeField, Range(0.1f, 5f)] private float sensitivity;
+
+    private float yaw = 0;
+    
+    void LateUpdate()
+    {
+        yaw += Input.GetAxis("Mouse X") * sensitivity;
+
+        Quaternion qYaw = Quaternion.AngleAxis(yaw, Vector3.up);
+        Quaternion qPitch = Quaternion.AngleAxis(pitch, Vector3.right);
+        Quaternion rotation = qYaw * qPitch;
+
+        Vector3 offset = rotation * Vector3.back * distance;
+
+        transform.position = target.position + offset;
+        transform.rotation = Quaternion.LookRotation(-offset);
+    }
+
+    public void SetTarget(Transform target)
+    {
+        this.target = target;
+        yaw = target.rotation.eulerAngles.y;
+    }
+}
--- Assets/Scripts/RollerPlayer.cs
+++ Assets/Scripts/RollerPlayer.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class RollerPlayer : MonoBehaviour
 {
+    [SerializeField] private Transform view;
     [SerializeField] private float maxForce = 5;
+
+    [SerializeField] private float groundRayLength = 1;
+    [SerializeField] private LayerMask groundLayer;
+
+    private int score = 0;
     private Vector3 force;
     private Rigidbody rb;
 
@@ -12,7 +18,14 @@ public class RollerPlayer : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-    }
+
+        view = Camera.main.transform;
+        Camera.main.GetComponent<RollerCamera>().SetTarget(transform);
+
+        GetComponent<Health>().onDamage += OnDamage;
+        GetComponent<Health>().onDeath += OnDeath;
+		RollerGameManager.Instance.SetHealth((int)GetComponent<Health>().health);
+	}
 
     // Update is called once per frame
     void Update()
@@ -22,9 +35,14 @@ public class RollerPlayer : MonoBehaviour
         direction.x = Input.GetAxis("Horizontal");
         direction.z = Input.GetAxis("Vertical");
 
-        force = direction * maxForce;
+        Quaternion viewSpace = Quaternion.AngleAxis(view.rotation.eulerAngles.y, Vector3.up);
+        force = viewSpace * (direction * maxForce);
 
-        if (Input.GetButtonDown("Jump")) 
+		Ray ray = new Ray(transform.position, Vector3.down);
+		bool onGround = Physics.Raycast(ray, groundRayLength, groundLayer);
+		Debug.DrawRay(transform.position, ray.direction * groundRayLength);
+
+		if (onGround && Input.GetButtonDown("Jump")) 
         {
             rb.AddForce(Vector3.up * 10, ForceMode.Impulse);
         }
@@ -34,4 +52,21 @@ public class RollerPlayer : MonoBehaviour
 	{
         rb.AddForce(force);	
 	}
+
+    public void AddPoints(int points)
+    {
+        score += points;
+        RollerGameManager.Instance.SetScore(score);
+    }
+
+    public void OnDamage()
+    {
+        Debug.Log("ouch");
+        RollerGameManager.Instance.SetHealth((int)GetComponent<Health>().health);
+    }
+
+    public void OnDeath()
+    {
+        RollerGameManager.Instance.SetGameOver();
+    }
 }
